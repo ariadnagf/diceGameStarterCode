9a23d19120208df2f4f9a2882236432db2e26b4c
ariadna <gomezariadna.ag@gmail.com>
2017-03-29 21:51:46 +0000

    still completing ML1

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
+  $("button").click(function(){
+    var name = $("#input").val();
+  });
+  $("body").append(rolldice());
+ $("#roll-dice").click(function() {
+    
+  });
   function rolldice(){
-  //Place your code here 
-
-
+  //Place your code here
+  return  Math.floor(Math.random() * 6 ) +1
+  
   };
   
 });
